2269b1634ceb17774a730a90a2ab5e847276a6c6
spericolato <salhiabdennour@yandex.com>
2021-07-06 10:27:31 -0700

    Gallery done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 const service = document.querySelectorAll(".serDiv");
 const contBtn = document.createElement("button");
+const send = document.getElementById("send");
+
+
 contBtn.innerHTML = "Contact!"
+
+
 contBtn.setAttribute("class","onCnt")
 service.forEach(element => {
     element.addEventListener("mouseover",()=>{
         element.appendChild(contBtn)
         contBtn.addEventListener("click",()=>{
             window.open("popup.html")
-
         })
     })
 });
+
+send.addEventListener("click",()=>{
+    alert("votre e-mail a été envoyé")
+})
