a572e3eee23a15702cb3095be3da7d085625eb9d
Spericolato <salhiabdennour@yandex.com>
2021-07-10 04:57:29 +1300

    hehe

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
+//service button adding & removing
+
 const service = document.querySelectorAll(".serDiv");
 const contBtn = document.createElement("button");
-const send = document.getElementById("send");
-
 
 contBtn.innerHTML = "Contact!"
-
-
 contBtn.setAttribute("class","onCnt")
+
 service.forEach(element => {
     element.addEventListener("mouseover",()=>{
         element.appendChild(contBtn)
         contBtn.addEventListener("click",()=>{
             window.open("popup.html")
+
         })
+    })
+    element.addEventListener("mouseleave",()=>{
+        element.removeChild(contBtn)
     })
 });
 
-send.addEventListener("click",()=>{
-    alert("votre e-mail a été envoyé")
+//setting up gallery
+
+const photos = document.querySelectorAll(".image");
+
+photos.forEach(item =>{
+    item.addEventListener("click",()=>{
+        window.open(item.getAttribute("src"))
+    })
 })
+
+//Send mail
+
